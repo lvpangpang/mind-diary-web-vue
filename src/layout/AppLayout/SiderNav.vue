3ddb9efc54df37ad1f246b5a50a0656dfc5aaadb
lvpangpang <574818075@qq.com>
2022-10-20 16:30:14 +0800

    feat:解决SideNav组件问题

--- src/layout/AppLayout/SiderNav.vue
+++ src/layout/AppLayout/SiderNav.vue
@@ -12,7 +12,7 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
 import { findParent } from './tools'
 
@@ -21,15 +21,24 @@ const props = defineProps(['sideNavMenu'])
 const openKeys = ref([])
 const selectedKeys = ref(null)
 const route = useRoute()
-if (props.sideNavMenu.length > 0) {
-  const list = findParent(props.sideNavMenu, route.fullPath)
-  if (list?.length === 2) {
-    const level2List = list[0]
-    const level3List = list[1]
-    openKeys.value = [level2List["id"] + ""]
-    selectedKeys.value = level3List["id"]
+let isMounted = true
+
+// React中props变化，整个函数都会执行一遍，但是vue中却不是这样的，所以需要用watch来监听变化
+watch(
+  () => props.sideNavMenu,
+  (menu) => {
+    if (menu.length > 0 && isMounted) {
+      const list = findParent(menu, route.fullPath)
+      if (list?.length === 2) {
+        const level2List = list[0]
+        const level3List = list[1]
+        openKeys.value = [level2List["id"] + ""]
+        selectedKeys.value = level3List["id"]
+      }
+      isMounted = false
+    }
   }
-}
+)
 
 const history = useRouter()
 const siderClick = (data) => {
